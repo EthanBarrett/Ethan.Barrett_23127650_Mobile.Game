e77d46cd1845bb3823ee9f327b007e0bb2f1fc8d
EthanBarrett <145779498+EthanBarrett@users.noreply.github.com>
2025-10-16 15:21:14 +0100

    added movement with joystick

--- Assets/Game/Script/PlayerMove.cs
+++ Assets/Game/Script/PlayerMove.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Vector2 move;
+    public float speed = 5f;
+
+
+    public void InputPlayer(InputAction.CallbackContext _context)
     {
-        
+        move = _context.ReadValue<Vector2>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 Player = new Vector3(move.x, 0, move.y);
+        Player.Normalize();
+        transform.Translate(speed * Player * Time.deltaTime);
     }
 }
