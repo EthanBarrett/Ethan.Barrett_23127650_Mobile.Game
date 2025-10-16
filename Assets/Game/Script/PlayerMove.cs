63c0523a974130047cb23ab2b21155adfbf165f5
EthanBarrett <145779498+EthanBarrett@users.noreply.github.com>
2025-10-16 16:31:15 +0100

    camera follow player

--- Assets/Game/Script/PlayerMove.cs
+++ Assets/Game/Script/PlayerMove.cs
@@ -19,5 +19,13 @@ public class PlayerMove : MonoBehaviour
         Vector3 Player = new Vector3(move.x, 0, move.y);
         Player.Normalize();
         transform.Translate(speed * Player * Time.deltaTime);
+
+       // if(Player != Vector3.zero)
+     //   {
+       //     float angle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
+       //     transform.rotation = Quaternion.Euler(0, angle, 0);
+       // }
+        
+
     }
 }
